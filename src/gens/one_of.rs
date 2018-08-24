2dd85209abe04dbf8d40f1f726e986fba5984214
Jakob Schikowski <jakob.schikowski@gmx.de>
2018-08-24 22:02:45 +0200

    Fix function names of generators `one_of_gen_*`

--- src/gens/one_of.rs
+++ src/gens/one_of.rs
@@ -56,25 +56,25 @@ macro_rules! fn_one_of_n {
     )
 }
 
-fn_one_of_n! { 2, one_of_2_once, one_of_2, one_of_gen_2, one_of_gen_2_once:
+fn_one_of_n! { 2, one_of_2_once, one_of_2, one_of_gen_2_once, one_of_gen_2:
     0, value_1, gen_1
     1, value_2, gen_2
 }
 
-fn_one_of_n! { 3, one_of_3_once, one_of_3, one_of_gen_3, one_of_gen_3_once:
+fn_one_of_n! { 3, one_of_3_once, one_of_3, one_of_gen_3_once, one_of_gen_3:
     0, value_1, gen_1
     1, value_2, gen_2
     2, value_3, gen_3
 }
 
-fn_one_of_n! { 4, one_of_4_once, one_of_4, one_of_gen_4, one_of_gen_4_once:
+fn_one_of_n! { 4, one_of_4_once, one_of_4, one_of_gen_4_once, one_of_gen_4:
     0, value_1, gen_1
     1, value_2, gen_2
     2, value_3, gen_3
     3, value_4, gen_4
 }
 
-fn_one_of_n! { 5, one_of_5_once, one_of_5, one_of_gen_5, one_of_gen_5_once:
+fn_one_of_n! { 5, one_of_5_once, one_of_5, one_of_gen_5_once, one_of_gen_5:
     0, value_1, gen_1
     1, value_2, gen_2
     2, value_3, gen_3
@@ -82,7 +82,7 @@ fn_one_of_n! { 5, one_of_5_once, one_of_5, one_of_gen_5, one_of_gen_5_once:
     4, value_5, gen_5
 }
 
-fn_one_of_n! { 6, one_of_6_once, one_of_6, one_of_gen_6, one_of_gen_6_once:
+fn_one_of_n! { 6, one_of_6_once, one_of_6, one_of_gen_6_once, one_of_gen_6:
     0, value_1, gen_1
     1, value_2, gen_2
     2, value_3, gen_3
@@ -91,7 +91,7 @@ fn_one_of_n! { 6, one_of_6_once, one_of_6, one_of_gen_6, one_of_gen_6_once:
     5, value_6, gen_6
 }
 
-fn_one_of_n! { 7, one_of_7_once, one_of_7, one_of_gen_7, one_of_gen_7_once:
+fn_one_of_n! { 7, one_of_7_once, one_of_7, one_of_gen_7_once, one_of_gen_7:
     0, value_1, gen_1
     1, value_2, gen_2
     2, value_3, gen_3
@@ -101,7 +101,7 @@ fn_one_of_n! { 7, one_of_7_once, one_of_7, one_of_gen_7, one_of_gen_7_once:
     6, value_7, gen_7
 }
 
-fn_one_of_n! { 8, one_of_8_once, one_of_8, one_of_gen_8, one_of_gen_8_once:
+fn_one_of_n! { 8, one_of_8_once, one_of_8, one_of_gen_8_once, one_of_gen_8:
     0, value_1, gen_1
     1, value_2, gen_2
     2, value_3, gen_3
@@ -112,7 +112,7 @@ fn_one_of_n! { 8, one_of_8_once, one_of_8, one_of_gen_8, one_of_gen_8_once:
     7, value_8, gen_8
 }
 
-fn_one_of_n! { 9, one_of_9_once, one_of_9, one_of_gen_9, one_of_gen_9_once:
+fn_one_of_n! { 9, one_of_9_once, one_of_9, one_of_gen_9_once, one_of_gen_9:
     0, value_1, gen_1
     1, value_2, gen_2
     2, value_3, gen_3
