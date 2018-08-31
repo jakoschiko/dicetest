9656c6992985c9314aa5dc671e3d104c369f852f
Jakob Schikowski <jakob.schikowski@gmx.de>
2018-08-31 19:28:10 +0200

    Implement `Debug` and `Clone` for `prop::Params`

--- src/prop/params.rs
+++ src/prop/params.rs
@@ -1,6 +1,7 @@
 use ::gen;
 
 /// These parameters configure the evaluation of properties. They will be passed to `Prop::eval`.
+#[derive(Debug, Clone)]
 pub struct Params {
     /// If set to `true`, `Label`s will be created and appended to the `Result`.
     pub create_labels: bool,
