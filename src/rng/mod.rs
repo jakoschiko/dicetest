24bc83faa2c5e60aa726b386629a7cce362b135e
Jakob Schikowski <jakob.schikowski@gmx.de>
2018-09-21 20:09:45 +0200

    Remove dead code

--- src/rng/mod.rs
+++ src/rng/mod.rs
@@ -1,7 +1,5 @@
 //! Provides a random number generator for simple integer numbers.
 //! This generator is the base for more complex generators that generate different kind of data.
 
-// mod conversion;
-
 mod rng;
 pub use self::rng::Rng;
