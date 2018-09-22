4ab657703c87b6b210f70a1e08317d8e2aafd528
Jakob Schikowski <jakob.schikowski@gmx.de>
2018-09-22 15:26:12 +0200

    Improve doc

--- src/prop/status.rs
+++ src/prop/status.rs
@@ -2,9 +2,12 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Status {
     /// The property was proven to be true.
+    ///
+    /// A property should either never or always evaluate to `True`.
     True,
-    /// The property withstood a test without being falsified. Useful for describing the result of
-    /// a partially evaluated universal quantifier.
+    /// The property withstood a test without being falsified.
+    ///
+    /// Useful for describing the result of a partially evaluated universal quantifier.
     Passed,
     /// The property was proven to be false.
     False,
