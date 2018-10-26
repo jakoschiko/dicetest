d621ef5d1ada01ad4c8884223773379b59e6e46d
Jakob Schikowski <jakob.schikowski@gmx.de>
2018-10-26 19:38:47 +0200

    Remove unnecessary type arguments

--- src/gens/vec.rs
+++ src/gens/vec.rs
@@ -15,7 +15,7 @@ impl<T> CollectionBuilder<T, Vec<T>> for VecBuilder {
 /// Generates a `Vec` that contains elements of type `T`.
 ///
 /// The range specifies the length of the `Vec`.
-pub fn vec<T, G, R>(
+pub fn vec<T>(
     elem_gen: impl Gen<T>,
     len_range: impl SizeRange,
 ) -> impl Gen<Vec<T>> {
