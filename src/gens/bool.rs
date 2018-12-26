c8f0458d1464596556aeff5db8b31a13ac6de410
Jakob Schikowski <jakob.schikowski@gmx.de>
2018-12-27 00:22:54 +0100

    Add `weighted_bool`

--- src/gens/bool.rs
+++ src/gens/bool.rs
@@ -2,5 +2,10 @@ use crate::prelude::gens::*;
 
 /// Generates `true` or `false` with the same probability.
 pub fn bool() -> impl Gen<bool> {
-    gens::one_of_2(true, false)
+    gens::one_of_2(false, true)
+}
+
+/// Generates `true` or `false` with probabilities based on the given weights.
+pub fn weighted_bool(false_weight: u32, true_weight: u32) -> impl Gen<bool> {
+    gens::weighted_one_of_2((false_weight, false), (true_weight, true))
 }
