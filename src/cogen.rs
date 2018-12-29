d8401858a7bb7ac50ad3bc0d0f7f67341945a7d4
Jakob Schikowski <jakob.schikowski@gmx.de>
2018-12-29 14:56:40 +0100

    Remove `Cogen` implementation for `Fn`

--- src/cogen.rs
+++ src/cogen.rs
@@ -4,12 +4,3 @@ pub trait Cogen<T> {
     /// Converts the given value into a seed. The implementation must be deterministic.
     fn cogen(&self, value: T) -> u64;
 }
-
-impl<T, F> Cogen<T> for F
-where
-    F: Fn(T) -> u64,
-{
-    fn cogen(&self, value: T) -> u64 {
-        self(value)
-    }
-}
--- src/cogens.rs
+++ src/cogens.rs
@@ -5,13 +5,32 @@ use std::collections::hash_map::DefaultHasher;
 
 use crate::prelude::cogens::*;
 
+struct Fun<F>(F);
+
+impl<T, F> Cogen<T> for Fun<F>
+where
+    F: Fn(T) -> u64,
+{
+    fn cogen(&self, value: T) -> u64 {
+        self.0(value)
+    }
+}
+
+/// Helper for implementing a `Cogen` from a `Fn` that returns a seed.
+pub fn from_fn<T, F>(f: F) -> impl Cogen<T>
+where
+    F: Fn(T) -> u64,
+{
+    Fun(f)
+}
+
 /// Uses stdlib's `DefaultHasher` to create a seed from a hashable value.
 pub fn from_default_hasher<T: Hash>() -> impl Cogen<T> {
-    |value: T| {
+    from_fn(|value: T| {
         let mut hasher = DefaultHasher::new();
         value.hash(&mut hasher);
         hasher.finish()
-    }
+    })
 }
 
 #[cfg(test)]
