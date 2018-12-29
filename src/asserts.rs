3cef059021f747d6423409de96237f8584c9bd9d
Jakob Schikowski <jakob.schikowski@gmx.de>
2018-12-29 12:14:32 +0100

    Remove `asserts` folder

--- src/asserts.rs
+++ src/asserts.rs
@@ -1,4 +1,38 @@
 //! A collection of useful assertions.
 
-mod inverse;
-pub use self::inverse::*;
+use std::fmt::Debug;
+
+use crate::prelude::asserts::*;
+
+/// Asserts that `g` is a left inverse for `f`.
+pub fn left_inverse<X, Y>(
+    dice: &mut Dice,
+    gen_x: impl GenOnce<X>,
+    f: impl FnOnce(X) -> Y,
+    g: impl FnOnce(Y) -> X,
+)
+where
+    X: Debug + Clone + PartialEq,
+{
+    let x = gen_x.gen_once(dice);
+    let y = f(x.clone());
+    let other_x = g(y);
+
+    assert_eq!(x, other_x)
+}
+
+/// Asserts that `h` is a right inverse for `f`.
+pub fn right_inverse<X, Y>(
+    dice: &mut Dice,
+    gen_y: impl GenOnce<Y>,
+    f: impl FnOnce(X) -> Y,
+    h: impl FnOnce(Y) -> X,
+)
+where
+    Y: Debug + Clone + PartialEq,
+{
+    let y = gen_y.gen_once(dice);
+    let x = h(y.clone());
+    let other_y = f(x);
+
+    assert_eq!(y, other_y) }
