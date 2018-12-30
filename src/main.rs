88aea990ea9a29eaed74b797d9cc7a372453c17f
Jakob Schikowski <jakob.schikowski@gmx.de>
2018-12-30 15:36:35 +0100

    Add main with example

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+fn bubble_sort<T: Ord>(slice: &mut [T]) {
+    let len = slice.len();
+
+    for _ in 0..len {
+        for j in 1..len - 1 {
+            let jpp = j + 1;
+            if slice[j] > slice[jpp] {
+                slice.swap(j, jpp);
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut v: Vec<u8> = vec![0, 2, 2, 4, 1, 0, 2, 5];
+    println!("unsorted: {:?}", v);
+    bubble_sort(&mut v);
+    println!("  sorted: {:?}", v);
+}
+
+#[cfg(test)]
+mod tests {
+    use dicetest::prelude::tests::*;
+
+    use super::*;
+
+    #[ignore]
+    #[test]
+    fn result_of_bubble_sort_is_sorted() {
+        dicetest!(|dice| {
+            let mut v = gens::vec(gens::u8(..), ..).gen(dice);
+            hint_format!("unsorted: {:?}", v);
+
+            bubble_sort(&mut v);
+            hint_format!("  sorted: {:?}", v);
+
+            let is_sorted = v.windows(2).all(|w| w[0] <= w[1]);
+            assert!(is_sorted);
+        })
+    }
+}
