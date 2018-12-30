c14db95bd83e542c929f24a7df93d4c13d5d594f
Jakob Schikowski <jakob.schikowski@gmx.de>
2018-12-30 14:46:49 +0100

    Improve doc

--- src/gens/prng.rs
+++ src/gens/prng.rs
@@ -1,11 +1,11 @@
 use crate::prelude::gens::*;
 
-// Generates the next number of the `Prng` passed to the generator method.
+// Generates the next number of the `Prng` that will be passed to the generator.
 pub fn prng_next_number() -> impl Gen<u64> {
     gens::from_fn(|dice| dice.prng.next_number())
 }
 
-// Generates a fork of the `Prng` passed to the generator method.
+// Generates a fork of the `Prng` that will be passed to the generator.
 pub fn prng_fork() -> impl Gen<Prng> {
     gens::from_fn(|dice| dice.prng.fork())
 }
