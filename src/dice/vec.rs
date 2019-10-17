6f946ec3550b55aa9d7daa4f19b1e1aa0bec4fd9
Jakob Schikowski <jakob.schikowski@gmx.de>
2019-10-17 23:37:49 +0200

    Add `dice::split_vec`

--- src/dice/vec.rs
+++ src/dice/vec.rs
@@ -56,6 +56,32 @@ pub fn shuffled_vec<T>(mut vec: Vec<T>) -> impl DieOnce<Vec<T>> {
     })
 }
 
+/// Generates two `Vecs` by splitting the given `Vec` at a random index.
+///
+/// # Examples
+///
+/// ```
+/// use dicetest::prelude::dice::*;
+///
+/// let vec = vec![1, 2, 3, 4];
+/// let (prefix, suffix) = dice::split_vec(vec.clone()).sample_once();
+/// assert!(vec.starts_with(&prefix));
+/// assert!(vec.ends_with(&suffix));
+///
+/// let empty_vec: Vec<u64> = vec![];
+/// let (empty_prefix, empty_suffix) = dice::split_vec(empty_vec).sample_once();
+/// assert!(empty_prefix.is_empty());
+/// assert!(empty_suffix.is_empty());
+/// ```
+pub fn split_vec<T>(mut vec: Vec<T>) -> impl DieOnce<(Vec<T>, Vec<T>)> {
+    let index_die = dice::uni_usize(0..=vec.len());
+    dice::from_fn_once(move |fate| {
+        let at = index_die.roll(fate);
+        let other_vec = vec.split_off(at);
+        (vec, other_vec)
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use std::collections::HashMap;
@@ -83,6 +109,19 @@ mod tests {
             hint_debug!(shuffled_vec);
 
             assert_eq!(orig_vec_elems, shuffled_vec_elems);
+        })
+    }
+
+    #[test]
+    fn split_vec_result_can_be_merged_to_orig_vec() {
+        dicetest!(|fate| {
+            let orig_vec = dice::vec(dice::u8(..), ..).roll(fate);
+            let (prefix, mut suffix) = dice::split_vec(orig_vec.clone()).roll_once(fate);
+
+            let mut merged = prefix;
+            merged.append(&mut suffix);
+
+            assert_eq!(merged, orig_vec);
         })
     }
 }
