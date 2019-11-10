827c78112fe354f77ba90e5d965771f63ba96086
Jakob Schikowski <jakob.schikowski@gmx.de>
2019-11-10 23:36:35 +0100

    Replace crate `rand` with `getrandom`

--- src/prand/seed.rs
+++ src/prand/seed.rs
@@ -1,13 +1,22 @@
-use rand::{self, Rng};
+use getrandom::getrandom;
 
 /// A seed for pseudorandomness.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Seed(pub u64);
 
 impl Seed {
-    /// Creates a random seed.
+    /// Creates a random seed using the random number generator of the operating system.
+    ///
+    /// # Panics
+    ///
+    /// If the operation system fails to generate the needed amount of random bytes this function
+    /// will panic. See the documetation of [getrandom] for more details.
+    ///
+    /// [getrandom]: https://docs.rs/getrandom
     pub fn random() -> Self {
-        let seed = rand::thread_rng().gen();
+        let mut buf = [0u8; 8];
+        getrandom(&mut buf).expect("Random seed generation has failed");
+        let seed = u64::from_le_bytes(buf);
         Seed(seed)
     }
 }
