3fa062c6683b4806cdcceaf8c126567ed2d14b4f
Jakob Schikowski <jakob.schikowski@gmx.de>
2019-11-11 01:11:25 +0100

    Add compilation error for unsupported pointer widths

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,13 @@
 //! [README]: https://github.com/jakoschiko/dicetest/blob/master/README.md
 //! [GUIDE]: https://github.com/jakoschiko/dicetest/blob/master/GUIDE.md
 
+// This crate makes assumptions regarding the pointer width. The following conditional error
+// prevents the compilation for unsupported pointer widths.
+//
+// See https://github.com/rust-lang/rfcs/issues/1748
+#[cfg(not(any(target_pointer_width = "32", target_pointer_width = "64")))]
+compile_error!("Only targets with pointer width 32 and 64 are currently supported");
+
 mod macros;
 
 mod util;
